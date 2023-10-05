e28c139e82fa3283ce2c00d394c59d01e5605f7d
Miguel Ángel Durán <miduga@gmail.com>
2023-10-05 18:59:54 +0200

    Add code of spotify clone

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,7 +1,7 @@
 import { Pause, Play } from "./Player"
 import { usePlayerStore } from '@/store/playerStore'
 
-export function CardPlayButton ({ id }) {
+export function CardPlayButton ({ id, size = 'small' }) {
   const {
     currentMusic,
     isPlaying,
@@ -25,17 +25,14 @@ export function CardPlayButton ({ id }) {
         setIsPlaying(true)
         setCurrentMusic({ songs, playlist, song: songs[0] })
       })
-
-    // async/await
-    // const res = await fetch(`/api/get-info-playlist.json?id=${id}`)
-    // const data = await res.json()
-    // const { songs, playlist } = data
   }
+
+  const iconClassName = size === 'small' ? 'w-4 h-4' : 'w-5 h-5'
 
 
   return (
-    <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4">
-      {isPlayingPlaylist ? <Pause /> : <Play />}
+    <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400">
+      {isPlayingPlaylist ? <Pause className={iconClassName} /> : <Play className={iconClassName} />}
     </button>
   )
 }
