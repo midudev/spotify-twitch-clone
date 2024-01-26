bdb252d6ea306da44177dcda9c6a0be0911e73d0
Luis B <luis.beroiza@adesso.de>
2024-01-26 22:13:31 +0100

    fix hide picture component if there are not image

--- src/components/PlayerCurrentSong.jsx
+++ src/components/PlayerCurrentSong.jsx
@@ -1,14 +1,15 @@
-export const PlayerCurrentSong = ({ image, title, artists }) => {
+export const PlayerCurrentSong = ({image, title, artists}) => {
   return (
     <div
       className={`
         flex items-center gap-5 relative
         overflow-hidden
       `}>
-      <picture className="w-16 h-16 bg-zinc-800 rounded-md shadow-lg overflow-hidden">
-        <img src={image} alt={title} />
-      </picture>
-
+      { image &&
+      (<picture className="w-16 h-16 bg-zinc-800 rounded-md shadow-lg overflow-hidden">
+        <img src={image} alt={title}/>
+      </picture>)
+      }
       <div className="flex flex-col">
         <h3 className="font-semibold text-sm block">
           {title}
