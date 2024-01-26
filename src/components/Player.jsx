aa169e2f952f47137afe2379f08b6d412f791ad0
Luis B <luis.beroiza@adesso.de>
2024-01-26 21:34:23 +0100

    add ; to code

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,15 +1,14 @@
 import { usePlayerStore } from "@/store/playerStore"
-import { useEffect, useRef } from "react"
+import { Pause, Play } from "@/icons/PlayerIcons"
 import { PlayerCurrentSong } from "@/components/PlayerCurrentSong"
 import { PlayerSoundControl } from "@/components/PlayerSoundControl"
 import { PlayerVolumeControl } from "@/components/PlayerVolumeControl"
-import { Pause, Play } from "@/icons/PlayerIcons"
+import { useEffect, useRef } from "react"
 
 
-
-export function Player () {
-  const { currentMusic, isPlaying, setIsPlaying, volume } = usePlayerStore(state => state)
-  const audioRef = useRef()
+export function Player() {
+  const {currentMusic, isPlaying, setIsPlaying, volume} = usePlayerStore(state => state);
+  const audioRef = useRef();
 
   useEffect(() => {
     isPlaying
@@ -18,23 +17,20 @@ export function Player () {
   }, [isPlaying])
 
   useEffect(() => {
-    audioRef.current.volume = volume
+    audioRef.current.volume = volume;
   }, [volume])
 
   useEffect(() => {
-    const { song, playlist, songs } = currentMusic
+    const {song, playlist} = currentMusic;
     if (song) {
-      const src = `/music/${playlist?.id}/0${song.id}.mp3`
-      audioRef.current.src = src
-      audioRef.current.volume = volume
-      audioRef.current.play()
+      audioRef.current.src = `/music/${playlist?.id}/0${song.id}.mp3`;
+      audioRef.current.volume = volume;
+      audioRef.current.play();
     }
   }, [currentMusic])
 
   const handleClick = () => {
-    if(currentMusic.song) {
-      setIsPlaying(!isPlaying)
-    }
+    setIsPlaying(!isPlaying);
   }
 
   return (
@@ -46,15 +42,15 @@ export function Player () {
       <div className="grid place-content-center gap-4 flex-1">
         <div className="flex justify-center flex-col items-center">
           <button className="bg-white rounded-full p-2" onClick={handleClick}>
-            {isPlaying ? <Pause /> : <Play />}
+            {isPlaying ? <Pause/> : <Play/>}
           </button>
-          <PlayerSoundControl audio={audioRef} />
-          <audio ref={audioRef} />
+          <PlayerSoundControl audio={audioRef}/>
+          <audio ref={audioRef}/>
         </div>
       </div>
 
       <div className="grid place-content-center">
-        <PlayerVolumeControl />
+        <PlayerVolumeControl/>
       </div>
     </div>
   )
